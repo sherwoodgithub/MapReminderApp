5672b274ed9badc93f67623f78348ccebafefc58
sherwoodgithub <stephenemails@gmail.com>
2015-02-03 13:29:50 -0800

    Recovered after git disaster

--- ReminderMapApp/ViewController.h
+++ ReminderMapApp/ViewController.h
@@ -1,15 +1,26 @@
 //
 //  ViewController.h
-//  ReminderMapApp
+//  ReMap
 //
-//  Created by Stephen on 2/3/15.
+//  Created by Stephen on 2/2/15.
 //  Copyright (c) 2015 Sherwood. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    //UIButton *buttonOne;
+    //UIButton *buttonTwo;
+    //UIButton *buttonThree;
+    
+}
 
+@property CLLocationCoordinate2D locationToDisplay;
+@property MKCoordinateRegion viewRegion;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIButton *buttonOne;
+@property (weak, nonatomic) IBOutlet UIButton *buttonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *buttonThree;
 
 @end
-
--- ReminderMapApp/ViewController.m
+++ ReminderMapApp/ViewController.m
@@ -1,27 +1,104 @@
 //
 //  ViewController.m
-//  ReminderMapApp
+//  ReMap
 //
-//  Created by Stephen on 2/3/15.
+//  Created by Stephen on 2/2/15.
 //  Copyright (c) 2015 Sherwood. All rights reserved.
 //
 
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager * locationManager;
 
 @end
 
 @implementation ViewController
 
+-(void)viewWillAppear:(BOOL)animated {
+    //    [super viewWillAppear:true];
+    
+    _locationToDisplay.latitude = 47.613114;
+    _locationToDisplay.longitude = -122.344670;
+    
+    _viewRegion = MKCoordinateRegionMakeWithDistance(_locationToDisplay, 4000, 4000);
+    
+    [_mapView setRegion:_viewRegion animated:YES];
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    
+    //    if ([CLLocationManager authorizationStatus]) {
+    //
+    //    }
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+//-(void) setRegion {
+//    _viewRegion = MKCoordinateRegionMakeWithDistance(_locationToDisplay, 2000, 2000);
+//    [_mapView setRegion:_viewRegion animated:YES];
+//}
+
+//MARK: MapView
+
+
+
+//MARK: Location
+- (void) locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    NSLog(@"locationStatus : %d", status);
+}
+
+- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *newLocation = [locations lastObject];
+    CLLocation *oldLocation;
+    if (locations.count > 1) {
+        oldLocation = [locations objectAtIndex:locations.count -2];
+    } else {
+        oldLocation = nil;
+    }
+    NSLog(@"didUpdateLocations: from %@ to %@", oldLocation, newLocation);
+    MKCoordinateRegion userLocation = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, 2000, 2000);
+    [_mapView setRegion:userLocation animated:YES];
+}
+
+//MARK: button pressed
+- (IBAction)buttonOnePressed:(id)sender {
+    // subZero refrigerators
+    _locationToDisplay.latitude = 47.633779;
+    _locationToDisplay.longitude = -122.372448;
+    _viewRegion = MKCoordinateRegionMakeWithDistance(_locationToDisplay, 2000, 2000);
+    [_mapView setRegion:_viewRegion animated:YES];
+    NSLog(@"button 1 pressed");
+}
+- (IBAction)buttonTwoPressed:(id)sender {
+    // ice rink
+    _locationToDisplay.latitude = 47.761905;
+    _locationToDisplay.longitude = -122.346339;
+    _viewRegion = MKCoordinateRegionMakeWithDistance(_locationToDisplay, 2000, 2000);
+    [_mapView setRegion:_viewRegion animated:YES];
+    NSLog(@"button 2 pressed");
+    
+}
+- (IBAction)buttonThreePressed:(id)sender {
+    // arctic club
+    _locationToDisplay.latitude = 47.603750;
+    _locationToDisplay.longitude = -122.331695;
+    _viewRegion = MKCoordinateRegionMakeWithDistance(_locationToDisplay, 2000, 2000);
+    [_mapView setRegion:_viewRegion animated:YES];
+    NSLog(@"button 3 pressed");
 }
 
 @end

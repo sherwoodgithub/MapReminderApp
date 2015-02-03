917c640e708298ec811372290eaadcc631bcbf94
sherwoodgithub <stephenemails@gmail.com>
2015-02-03 13:25:41 -0800

    Initial Commit

---
+++ ReminderMapApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReminderMapApp
+//
+//  Created by Stephen on 2/3/15.
+//  Copyright (c) 2015 Sherwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
